5391f27b2d7a5f0581a6e9643992b2e3370372b7
Muqtxdir <muqtxdir@gmail.com>
2026-07-19 13:34:23 +0530

    build (dash): skeleton for dismiss same as popover

---
+++ src/components/unity-dismiss.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+/**
+ * UnityDismissFunc:
+ * @user_data: the data passed to unity_dismiss_attach()
+ *
+ * Callback invoked when the surface should dismiss.
+ */
+typedef void (*UnityDismissFunc) (gpointer user_data);
+
+void unity_dismiss_attach (GtkWidget        *surface,
+                           GtkWidget        *area,
+                           GtkWidget        *content,
+                           UnityDismissFunc  on_dismiss,
+                           gpointer          user_data);
+
+G_END_DECLS
